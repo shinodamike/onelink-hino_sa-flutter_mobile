8067791b228f2ce0be2df173f3e1d52927d3adc4
shinodamike <wanlaya.son@orbplus.co.th>
2025-03-22 13:15:31 +0700

    update library - flutter version 3.27.0 - dart 3.6.0

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,6 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyA_wu7uim5xPTlxsduNj1IMbdL4omnvybU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
